dfdb25b849b010b49d3df7cbb8b1c5c3411a7740
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2024-05-19 15:19:13 -0300

    delete generic context

--- src/app/examples/context/page.tsx
+++ src/app/examples/context/page.tsx
@@ -1,13 +1,10 @@
 import Container from '../../components/Expandable';
 import { GitHubUserContent } from '../../components/GitHubUserContent';
-import { GenericProvider } from '../../contexts/GenericContext';
 
 export default function Home() {
   return (
-    <GenericProvider>
-      <Container>
-        <GitHubUserContent />
-      </Container>
-    </GenericProvider>
+    <Container>
+      <GitHubUserContent />
+    </Container>
   );
 }
